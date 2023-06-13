154ac92530180ccb496412604714f96d031962d8
Confidence Madiope <confidencem97@gmail.com>
2023-06-13 18:51:18 +0200

    fixed about me page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import footer from "../../public/Email-Signature-V23_04_04.jpg";
 const Footer = () => {
   return (
     <footer className='bg-black'>
-      <Image src={footer} alt='footer' />
+      <Image src={footer} alt='footer' className="md:h-[400px]"/>
     </footer>
   );
 };
